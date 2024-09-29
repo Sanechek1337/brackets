339dbdb774acf60c40bc21d8bf414595d07fb11a
Alexandr <rednaxela659@gmail.com>
2024-09-29 20:50:36 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsMap = new Map(bracketsConfig);
+
+  const openStack = [];
+
+  for (let char of str) {
+    if (bracketsMap.has(char)) {
+      if (bracketsMap.get(char) === char) {
+        if (openStack[openStack.length - 1] === char) {
+          openStack.pop();
+        } else {
+          openStack.push(char);
+        }
+      } else {
+        openStack.push(char);
+      }
+    } else {
+      const lastOpen = openStack.pop();
+      if (bracketsMap.get(lastOpen) !== char) {
+        return false;
+      }
+    }
+  }
+
+  return openStack.length === 0;
+};
